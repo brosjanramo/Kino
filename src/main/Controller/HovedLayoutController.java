5f68dea71cee45010c907704769a26dd6bc82063
Simen <lsimen89@gmail.com>
2019-03-21 12:54:05 +0100

    Hovedlayout
    Lagt til kode i hovedlayout controller

--- src/main/Controller/HovedLayoutController.java
+++ src/main/Controller/HovedLayoutController.java
@@ -1,4 +1,22 @@
 package Controller;
 
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ListView;
+import java.awt.*;
+
 public class HovedLayoutController {
+
+    @FXML
+    private ListView evenList;
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    public void initialize() {
+
+
+    }
+
 }
