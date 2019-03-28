3d96e52d715b0c057410ef3c9d0c270ef7e81207
brosjanramo <32501499+brosjanramo@users.noreply.github.com>
2019-03-28 13:24:03 +0100

    tested json file dir
    tested json file dir

--- src/main/Gruppe10/Json/WriteJson.java
+++ src/main/Gruppe10/Json/WriteJson.java
@@ -12,7 +12,7 @@ public class WriteJson {
     public static void main (String[] args){
         JSONObject obj = new JSONObject();
         obj.put("name", "Test1");
-        obj.put("Mail", "Test@hiof.no");
+        obj.put("Mail", "Test@hiof.pk");
 
         JSONArray list = new JSONArray();
         list.add("sopptur");
@@ -21,7 +21,7 @@ public class WriteJson {
         obj.put("Events", list);
 
         try{
-            FileWriter file = new FileWriter("write.json");
+            FileWriter file = new FileWriter("json/write.json");
             file.write(obj.toString());
             file.flush();
             file.close();
