424bc275077f7f6b5077c700f333c6eeb3e8ea2e
ChristopherJLorentzen <acydc05@gmail.com>
2019-03-28 11:23:55 +0100

    Lagd skrive til Json
    Skriver til JsonObject og blir printet ut med System.out.println(obj);
    Men blir ikke lagret på fil

---
+++ src/main/JsonReader/WriteJson.java
@@ -0,0 +1,34 @@
+package JsonReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteJson {
+
+    @SuppressWarnings("unchecked")
+    public static void main (String[] args){
+        JSONObject obj = new JSONObject();
+        obj.put("name", "Test1");
+        obj.put("Mail", "Test@hiof.no");
+
+        JSONArray list = new JSONArray();
+        list.add("sopptur");
+        list.add("konsert");
+
+        obj.put("Events", list);
+
+        try{
+            FileWriter file = new FileWriter("write.json");
+            file.write(obj.toString());
+            file.flush();
+            file.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        System.out.println(obj);
+    }
+}
